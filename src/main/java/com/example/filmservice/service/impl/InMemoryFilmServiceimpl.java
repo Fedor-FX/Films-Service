5681fb97f1653f748ea0448b35952056e1e96f8f
Fedor-FX <fedorvouynkov@mail.ru>
2025-02-24 13:15:53 +0300

    Add files via upload

---
+++ src/main/java/com/example/filmservice/service/impl/InMemoryFilmServiceimpl.java
@@ -0,0 +1,25 @@
+package com.example.filmservice.service.impl;
+
+import com.example.filmservice.model.Film;
+import com.example.filmservice.repository.InMemoryFilmDao;
+import com.example.filmservice.service.FilmService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+
+@Service
+@AllArgsConstructor
+public class InMemoryFilmServiceimpl implements FilmService { // Добавляем implements FilmService
+
+    public final InMemoryFilmDao repository;
+
+    @Override
+    public Film findFilmById(int id) {
+        return repository.findFilmById(id);
+    }
+
+    @Override
+    public Film getFilmByTitle(String title) {
+        return repository.getFilmByTitle(title);
+    }
+}
