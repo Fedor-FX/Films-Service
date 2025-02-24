eaef3359b356882058fd1cbfa0c31d6cbcafdb3e
Fedor-FX <fedorvouynkov@mail.ru>
2025-02-24 23:15:24 +0300

    Add files via upload

---
+++ src/test/java/com/example/filmservice/FilmServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.filmservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+		// Тест загружает контекст приложения, проверка конфигурации Spring Boot.
+	}
+}
+
